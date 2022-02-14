d22fa623db53a97703c0c79a32e8b7a0ab4739aa
Usu2 Name <usu2@example.com>
2022-02-14 23:57:38 +0100

    Cambio2Cambio2

--- Proyecto1/src/cambio1/Cambio1.java
+++ Proyecto1/src/cambio1/Cambio1.java
@@ -5,6 +5,7 @@ public class Cambio1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Pruebas Git. Clase Cambio1");
+		System.out.println("Pruebas Git. Clase Cambio1");
 	}
 
 }
